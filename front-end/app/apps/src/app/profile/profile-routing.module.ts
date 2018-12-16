be7d2b1fca11ab456896a2e741d25b59b4af341b
Steve Huang <3016218116@tju.edu.cn>
2018-12-16 20:33:57 +0800

    solve some bugs

--- front-end/app/apps/src/app/profile/profile-routing.module.ts
+++ front-end/app/apps/src/app/profile/profile-routing.module.ts
@@ -3,16 +3,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 //import my components
 import { ProfileComponent } from './profile.component';
-import { ProfileDetailComponent } from './profile-detail/profile-detail.component';
 
 const routes: Routes = [
     {
         path: '',
         component: ProfileComponent
-    },
-    {
-        path:'profile-detail',
-        component: ProfileDetailComponent
     }
 ];
 
